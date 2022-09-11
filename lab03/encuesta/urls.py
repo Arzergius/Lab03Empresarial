d217f8868f070db8191a9d73fb4601c4530150ed
Arzergius <102237015+Arzergius@users.noreply.github.com>
2022-09-11 18:45:57 -0500

    Add files via upload

---
+++ lab03/encuesta/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name ='encuesta'
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('<int:pregunta_id>/',views.detalle,name='detalle'),
+    path('<int:pregunta_id>/votar',views.votar,name='votar'),
+    path('<int:pregunta.id>/votar',views.votar,name='votar')
+
+]
+
+
